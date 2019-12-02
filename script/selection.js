dffc13455d8f1adc9beb5cb2cc7e0265b1dc1913
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2019-12-02 15:14:25 +0100

    added js selection, changed the position of help button etc.

--- script/selection.js
+++ script/selection.js
@@ -1,1 +1,42 @@
+ /*jshint esversion: 8 */
+let player = null;
+let startGame = document.getElementById("startGame");
 
+let allPlayers = document.querySelectorAll(".charBox");
+
+allPlayers.forEach(function(playerCard) {
+
+  playerCard.addEventListener("click", function() {
+
+    let characterName = this.dataset.name;
+
+    let selectedPlayerDisplay = document.getElementById("selectedPlayer");
+
+    if (this.classList.contains("theChosen")) {
+      this.classList.remove("theChosen");
+      player = null;
+    } else {
+      allPlayers.forEach(function(playerCard) {
+        playerCard.classList.remove("theChosen");
+      });
+      this.classList.add("theChosen");
+      player = characterName;
+    }
+
+
+    if (player === null) {
+      selectedPlayerDisplay.innerHTML = "None selected";
+      localStorage.removeItem("player");
+      startGame.disabled = true;
+    } else {
+      selectedPlayerDisplay.innerHTML = characterName;
+      localStorage.setItem("player", characterName);
+      startGame.disabled = false;
+    }
+  });
+
+});
+
+startGame.addEventListener("click", function() {
+  window.location.href = "thegame.html";
+});
